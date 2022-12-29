161bb98cb78cc1106e2d0c48cb5cfd224561ac3f
viktorbeznosov <viktorbeznosov@mail.ru>
2022-12-29 22:52:17 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Viktor Beznosov");
+        System.out.println("29.12.2022");
+    }
+
+}
