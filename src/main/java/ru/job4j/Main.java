f456b9710bbd2fa8fd93311c9ef49f823a07cf07
viktorbeznosov <viktorbeznosov@mail.ru>
2022-12-30 20:26:09 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,23 +3,5 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-
-        int age = 18;
-        char sex = 'М';
-        float height = 1.76F;
-        long bricksCount = 20000000;
-        int bacteriaCount = 10000000;
-        long spaceDistance = 1000000000000L;
-        boolean workPermission = true;
-        long fileSize = 100000000000L;
-
-        System.out.println(age);
-        System.out.println(sex);
-        System.out.println(height);
-        System.out.println(bricksCount);
-        System.out.println(bacteriaCount);
-        System.out.println(spaceDistance);
-        System.out.println(workPermission);
-        System.out.println(fileSize);
     }
 }
