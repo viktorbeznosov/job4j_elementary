94d69af1e7d39a561cce4dae2ef1245fe09f1857
viktorbeznosov <viktorbeznosov@mail.ru>
2022-12-30 22:34:55 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -3,9 +3,9 @@ package ru.job4j.calculator;
 public class Calculator {
 
     public static void main(String[] args) {
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
+        plus(100, 500);
+        plus(4, 2);
+        plus(3, 5);
     }
 
     public static void plus(int first, int second) {
