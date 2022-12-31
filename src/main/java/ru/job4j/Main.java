2d4dda3d36d2b131cd7ee993d6a2bfbe4697db91
viktorbeznosov <viktorbeznosov@mail.ru>
2022-12-31 19:07:31 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,11 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        int one = 10;
+        double two = 100.0;
+        one += two;
+
+        System.out.println(one);
     }
 }
