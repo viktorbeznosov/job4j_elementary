1b4e1305a0efa9d43e03cb5866c94bb1b851915e
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-03 12:12:01 +0300

    4.1.2. Глупый бот [#31323 #376719]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -6,7 +6,7 @@ public class DummyBot {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
             rsl = "Hi, SmartAss.";
-        } else if ("Bye.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
+        } else if ("Bye.".equals(question)) {
             rsl = "See you later.";
         }
         return rsl;
