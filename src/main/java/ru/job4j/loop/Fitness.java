2c161f273dcc76a40d5d1e4929054a359b78bbdd
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-04 14:13:51 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #377191]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,9 +4,6 @@ public class Fitness {
 
     public static int calc(int ivan, int nik) {
         int month = 0;
-        if (ivan > nik) {
-            return month;
-        }
         while (nik >= ivan) {
             ivan *= 3;
             nik *= 2;
