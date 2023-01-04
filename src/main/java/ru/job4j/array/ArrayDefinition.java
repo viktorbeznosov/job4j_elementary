7bec41163e07751d13179cfd8fad3364a1a081a4
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-04 15:13:36 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива ages равен " + ages.length);
-        System.out.println("Размер массива surnames равен " + surnames.length);
-        System.out.println("Размер массива prices равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Viktor Beznosov";
+        names[2] = "Igor Sivolobov";
+        names[3] = "Ivan Shashin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
