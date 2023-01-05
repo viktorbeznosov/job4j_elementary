440784e1d097eef573ee4f187632b54321d89f68
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-05 11:23:01 +0300

    6.2.2. Переставить элементы массива [#226795 #377481]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -4,8 +4,7 @@ public class AlgoArray {
 
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
-        int tmp;
-        tmp = array[0];
+        int tmp = array[0];
         array[0] = array[3];
         array[3] = tmp;
         tmp = array[1];
--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -3,8 +3,7 @@ package ru.job4j.array;
 public class SwitchArray {
 
     public static int[] swap(int[] array, int source, int dest) {
-        int temp;
-        temp = array[source];
+        int temp = array[source];
         array[source] = array[dest];
         array[dest] = temp;
         return array;
